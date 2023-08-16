5b9c5d837c917fee62da42dd869b1923c06c3474
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-08-16 15:41:11 +0000

    Add webpack configurations files running at http://localhost:3000/

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+// const path = require('path');
+
+// module.exports = {
+//   mode: 'development',
+//   entry: './foo.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'foo.bundle.js',
+//   },
+// };
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+    template: './src/index.html',
+    title: 'Development',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
